e75f0d3b133f89f910cce22a4fc32a8cc38b0894
Ikechukwu <106882590+Ikechukwu980@users.noreply.github.com>
2024-10-20 02:21:23 -0500

    Update backend.tf

--- terraform-modules/backend.tf
+++ terraform-modules/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-serverlessapp"
+    bucket         = "ikeboy-1234"
     key            = "serverless-web-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-"
