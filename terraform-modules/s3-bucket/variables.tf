ce7e681322cbe6760df5b42f6d9f815ac64dee68
Ikechukwu <106882590+Ikechukwu980@users.noreply.github.com>
2024-10-20 02:49:25 -0500

    Update variables.tf

--- terraform-modules/s3-bucket/variables.tf
+++ terraform-modules/s3-bucket/variables.tf
@@ -1,19 +1,27 @@
+# variables.tf
+
 variable "bucket_name" {
-  description = "The name of the S3 bucket"
+  description = "The name of the S3 bucket for static website hosting"
   type        = string
 }
 
 variable "index_document" {
-  description = "The index document for the static website"
+  description = "The index document for the S3 bucket hosting"
   type        = string
+  default     = "index.html"  # Optional default value
 }
 
 variable "error_document" {
-  description = "The error document for the static website"
+  description = "The error document for the S3 bucket hosting"
   type        = string
+  default     = "error.html"  # Optional default value
 }
 
 variable "tags" {
   description = "Tags for the S3 bucket"
   type        = map(string)
+  default     = {
+    Name        = "StaticWebsiteBucket"
+    Environment = "Production"
+  }
 }
