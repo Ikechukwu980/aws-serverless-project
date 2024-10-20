1bb831d598d2d3a72dcecc35669edf8ae10ded08
Ikechukwu <106882590+Ikechukwu980@users.noreply.github.com>
2024-10-20 04:23:19 -0500

    Update main.tf

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -1,11 +1,15 @@
 # creating an s3 bucket
 module "s3_static_website" {
-  source         = "./s3-bucket"
+  source    = "./s3-bucket"
+  providers = {
+    aws = aws.us_east
+  }
   bucket_name    = var.bucket_name
   index_document = var.index_document
   error_document = var.error_document
   tags           = var.tags
 }
+
 
 output "s3_bucket_name" {
   value = module.s3_static_website.bucket_name
