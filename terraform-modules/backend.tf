066e90342a391579ed96aebea239720dee17f320
Ikechukwu <106882590+Ikechukwu980@users.noreply.github.com>
2024-10-20 02:44:31 -0500

    Update backend.tf

--- terraform-modules/backend.tf
+++ terraform-modules/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "ikeboy-1234"                  # S3 bucket for state storage
-    key            = "serverless-web-app/terraform.tfstate"  # State file key
-    region         = "us-east-1"                    # AWS region
-    dynamodb_table = "terraform-state-lock"         # Complete DynamoDB table name
-    encrypt        = true                           # Encrypt state file
+    bucket         = "ikeboy-1234"
+    key            = "terraform-module/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "Terraform-state-lock"
+    encrypt        = true
   }
 }
 
